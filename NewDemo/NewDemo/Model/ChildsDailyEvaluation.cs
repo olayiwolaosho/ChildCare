9825ba4a1805ec852e39aa43ae81acb45b8dcde0
Adekunle Alugbin <alugbin96@gmail.com>
2020-05-16 23:00:24 +0100

    First sample on Models and Firebase

---
+++ NewDemo/NewDemo/Model/ChildsDailyEvaluation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewDemo.Model
+{
+    class ChildsDailyEvaluation
+    {
+        public int Id { get; set; }
+        public string Behaiour { get; set; }
+        public string Habit { get; set; }
+        public string Medication { get; set; }
+
+        //Foreignkey
+        public int ChildId { get; set; }
+
+        public DateTime Day { get; set; }
+    }
+}
---
+++ NewDemo/NewDemo/Model/RegisterChildren.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewDemo.Model
+{
+    public class RegisterChildren
+    {
+        public int id { get; set; }
+        public string Childname { get; set; }
+        public string password { get; set; }
+        // public string Childpicture { get; set; }
+
+        //Foreignkey
+        public int AttendantId { get; set; }
+    }
+}
