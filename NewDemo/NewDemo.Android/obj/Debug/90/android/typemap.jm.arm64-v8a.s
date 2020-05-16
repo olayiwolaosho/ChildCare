8919e4b7099840bfb0dd1067eeb1852ade3c6cf9
EastCartel <greatchibyke2016@gmail.com>
2020-05-16 14:03:06 +0100

    initial-commit

---
+++ NewDemo/NewDemo.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data Hash: 12E9B018B031CCD8 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1296
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 339553
+	.include	"typemap.jm.inc"
---
+++ NewDemo/NewDemo/ParentLogin.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NewDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ParentLogin : ContentPage
+    {
+        public ParentLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void ParentSignIn_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+                //Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
